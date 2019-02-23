05cb6b6c94f52639c849df4680e6701c2169a5ce
memory-agape <memory.style.serv@gmail.com>
2019-02-23 21:28:40 +0900

    WIP commit

--- src/kernel/core/Accumulator.php
+++ src/kernel/core/Accumulator.php
@@ -5,9 +5,11 @@ use PHPJava\Core\JavaClass;
 
 trait Accumulator
 {
-    use BinaryReader;
-
     private $javaClass;
+    
+    /**
+     * @var \PHPJava\Core\JVM\Stream\BinaryReader
+     */
     private $reader;
     private $localStorage;
     private $pointer;
@@ -23,5 +25,65 @@ trait Accumulator
         $this->localStorage = $localStorage;
         $this->pointer = $pointer;
         return $this;
+    }
+
+    final public function read($bytes = 1)
+    {
+        return $this->reader->read($bytes);
+    }
+
+    public function readByte()
+    {
+        return $this->reader->readByte();
+    }
+
+    public function readUnsignedByte()
+    {
+        return $this->reader->readUnsignedByte();
+    }
+
+    public function readUnsignedInt()
+    {
+        return $this->reader->readUnsignedInt();
+    }
+
+    public function readUnsignedShort()
+    {
+        return $this->reader->readUnsignedShort();
+    }
+
+    public function readInt()
+    {
+        return $this->reader->readInt();
+    }
+
+    public function readShort()
+    {
+        return $this->reader->readShort();
+    }
+
+    public function readUnsignedLong()
+    {
+        return $this->reader->readUnsignedLong();
+    }
+
+    public function readLong()
+    {
+        return $this->reader->readLong();
+    }
+
+    public function seek($bytes)
+    {
+        $this->reader->seek($bytes);
+    }
+
+    public function setOffset($pointer)
+    {
+        $this->reader->setOffset($pointer);
+    }
+
+    public function getOffset()
+    {
+        return $this->reader->getOffset();
     }
 }
