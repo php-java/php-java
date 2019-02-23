decd6efe18ad58eaa272862d235d3dcbc13318f5
memory-agape <memory.style.serv@gmail.com>
2019-02-23 12:20:05 +0900

    Update

---
+++ src/kernel/maps/AccessFlagEnum.php
@@ -0,0 +1,13 @@
+<?php
+namespace PHPJava\Kernel\Maps;
+
+class AccessFlagEnum extends Map
+{
+    const _Public     = 0x0001;
+    const _Private    = 0x0002;
+    const _Protected  = 0x0004;
+    const _Static     = 0x0008;
+    const _Final      = 0x0010;
+    const _Volatile   = 0x0040;
+    const _Transient  = 0x0080;
+}
---
+++ src/kernel/maps/ClassConstantEnum.php
@@ -0,0 +1,17 @@
+<?php
+namespace PHPJava\Kernel\Maps;
+
+class ClassConstantEnum extends Map
+{
+    const _Class =              0x0007;  
+    const _Fieldref =           0x0009;
+    const _Methodref =          0x000A;
+    const _InterfaceMethodref = 0x000B;
+    const _String =             0x0008;
+    const _Integer =            0x0003;
+    const _Float =              0x0004;
+    const _Long =               0x0005;
+    const _Double =             0x0006;
+    const _NameAndType =        0x000C;
+    const _Utf8 =               0x0001;
+}
