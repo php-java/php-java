a5658476c843593e5ae83f618ad86ec726f1069f
memory-agape <memory.style.serv@gmail.com>
2019-03-03 11:54:56 +0900

    Fix missed refactoring

--- src/Core/JavaClassFileReader.php
+++ src/Core/JavaClassFileReader.php
@@ -3,7 +3,7 @@ namespace PHPJava\Core;
 
 use PHPJava\Utilities\ClassResolver;
 
-class JavaClassFileReaderInterface implements JavaClassReaderInterface
+class JavaClassFileReader implements JavaClassReaderInterface
 {
     private $handle;
     private $binaryReader;
