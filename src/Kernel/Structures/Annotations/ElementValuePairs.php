f2c91e6e91356310150f354934675d562e823512
memory-agape <memory.style.serv@gmail.com>
2019-04-06 01:50:59 +0900

    WIP

--- src/Kernel/Structures/Annotations/ElementValuePairs.php
+++ src/Kernel/Structures/Annotations/ElementValuePairs.php
@@ -2,6 +2,7 @@
 namespace PHPJava\Kernel\Structures\Annotations;
 
 use PHPJava\Exceptions\NotImplementedException;
+use PHPJava\Exceptions\RuntimeException;
 use PHPJava\Kernel\Attributes\AttributeInfo;
 use PHPJava\Kernel\Attributes\RuntimeVisibleAnnotationsAttribute;
 use PHPJava\Utilities\BinaryTool;
@@ -12,7 +13,11 @@ final class ElementValuePairs implements AnnotationInterface
     use \PHPJava\Kernel\Core\ConstantPool;
 
     private $tag;
+    private $value;
 
+    /**
+     * @see https://docs.oracle.com/javase/specs/jvms/se11/html/jvms-4.html#jvms-4.7.16
+     */
     public function execute(): void
     {
         $this->tag = chr($this->readByte());
@@ -27,14 +32,36 @@ final class ElementValuePairs implements AnnotationInterface
             case 'S':
             case 'Z':
             case 's':
+                $this->value = $this->readUnsignedShort();
                 break;
             case 'e': // enum_const_value
+                $this->value = [
+                    'type_name_index' => $this->readUnsignedShort(),
+                    'const_name_index' => $this->readUnsignedShort(),
+                ];
                 break;
             case 'c': // class_info_index
+                $this->value = $this->readUnsignedShort();
                 break;
             case '@': // annotation_value
+                $this->value = new AttributeInfo($this->reader);
+                $this->value->setConstantPool($this->getConstantPool());
+                $this->value->execute();
                 break;
             case '[': // array_value
+                $this->value = [
+                    'num_values' => $this->readUnsignedShort(),
+                    'values' => [],
+                ];
+                for ($i = 0; $i < $this->value['num_values']; $i++) {
+                    $value = new static($this->reader);
+                    $value->setConstantPool($this->getConstantPool());
+                    $value->execute();
+                    $this->value['values'][] = $value;
+                }
+                break;
+            default:
+                throw new RuntimeException('Invalid tag "' . $this->tag . '" in element_value structure.');
                 break;
         }
 //
