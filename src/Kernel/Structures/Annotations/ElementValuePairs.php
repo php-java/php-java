97403cc00e8be659626a4a1d31baca6d38b14739
memory-agape <memory.style.serv@gmail.com>
2019-04-06 01:37:27 +0900

    WIP

--- src/Kernel/Structures/Annotations/ElementValuePairs.php
+++ src/Kernel/Structures/Annotations/ElementValuePairs.php
@@ -17,34 +17,62 @@ final class ElementValuePairs implements AnnotationInterface
     {
         $this->tag = chr($this->readByte());
 
-        $this->constValueIndex = $this->readUnsignedShort();
-
-
-        $this->enumConstValue = [
-            'type_name_index' => $this->readUnsignedShort(),
-            'const_name_index' => $this->readUnsignedShort(),
-        ];
-
-        $this->classInfoIndex = $this->readUnsignedShort();
-
-        $cpInfo = $this->getConstantPool()->getEntries();
-
-        if ($this->tag === '@') {
-            $this->annotationValue = new AttributeInfo($this->reader);
-            $this->annotationValue->setConstantPool($this->getConstantPool());
-            $this->annotationValue->execute();
-        }
-
-        if ($this->tag === '[') {
-            $this->arrayValue = [
-                'num_values' => $this->readUnsignedShort(),
-
-            ];
+        switch ($this->tag) {
+            case 'B': // const_value_index
+            case 'C':
+            case 'D':
+            case 'F':
+            case 'I':
+            case 'J':
+            case 'S':
+            case 'Z':
+            case 's':
+                break;
+            case 'e': // enum_const_value
+                break;
+            case 'c': // class_info_index
+                break;
+            case '@': // annotation_value
+                break;
+            case '[': // array_value
+                break;
         }
 //
-//        for ($i = 0; $i < $this->arrayValue['num_values']; $i++) {
+//        var_dump($this->tag);
 //
+//        $this->constValueIndex = $this->readUnsignedShort();
+//
+//
+//        $this->enumConstValue = [
+//            'type_name_index' => $this->readUnsignedShort(),
+//            'const_name_index' => $this->readUnsignedShort(),
+//        ];
+//
+//        $this->classInfoIndex = $this->readUnsignedShort();
+//
+//        $cpInfo = $this->getConstantPool()->getEntries();
+//
+//        if ($this->tag === '@') {
+//            $this->annotationValue = new AttributeInfo($this->reader);
+//            $this->annotationValue->setConstantPool($this->getConstantPool());
+//            $this->annotationValue->execute();
 //        }
+//
+//        if ($this->tag === '[') {
+//            $this->arrayValue = [
+//                'num_values' => $this->readUnsignedShort(),
+//
+//            ];
+//            for ($i = 0; ) {
+//
+//            }
+//            var_dump($this->arrayValue);
+//            exit();
+//        }
+////
+////        for ($i = 0; $i < $this->arrayValue['num_values']; $i++) {
+////
+////        }
 
     }
 }
