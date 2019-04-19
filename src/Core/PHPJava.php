b93f17752665d0974953cbf951be4291d6b0fcb4
memory-agape <memory.style.serv@gmail.com>
2019-04-19 17:14:47 +0900

    Add code name

---
+++ src/Core/PHPJava.php
@@ -0,0 +1,16 @@
+<?php
+namespace PHPJava\Core;
+
+final class PHPJava
+{
+    /**
+     * The code name is based on language of flowers.
+     * It meaning currently is "Expectation".
+     */
+    const CODE_NAME = 'ZEPHYRANTHES';
+
+    /**
+     * As same as composer version.
+     */
+    const VERSION = 0x000063;
+}
