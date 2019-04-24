fb87da58680ee8654027d100b43dccda4ed83ba4
memory-agape <memory.style.serv@gmail.com>
2019-04-24 19:40:05 +0900

    Add bi

---
+++ bin/PHPJava.php
@@ -0,0 +1,17 @@
+<?php
+
+use PHPJava\Core\JavaArchive;
+use PHPJava\Core\PHPJava;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+if (!isset($argv[1])) {
+    echo "First parameter is needed.\n";
+    return;
+}
+
+$jar = new JavaArchive($argv[1]);
+$arguments = array_slice($argv, 2);
+$jar->execute(
+    []
+);
