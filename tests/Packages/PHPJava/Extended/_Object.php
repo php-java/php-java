c68037abecd6f42f3534b3534309e235d2ecb377
Chitoku <odango@chitoku.jp>
2019-05-07 11:55:04 +0900

    Fix not to have a class definition only for test purpose

--- tests/Packages/PHPJava/Extended/_Object.php
+++ tests/Packages/PHPJava/Extended/_Object.php
@@ -4,19 +4,15 @@ namespace PHPJava\Tests\Packages\PHPJava\Extended\_Object;
 use PHPUnit\Framework\TestCase;
 use PHPJava\Packages\java\lang\CloneNotSupportedException;
 
-class DummyObject
-{
-    use \PHPJava\Packages\PHPJava\Extended\_Object;
-}
-
 class _ObjectTest extends Base
 {
     public function testClone()
     {
-        $this->expectException(CloneNotSupportedException);
-        $target = new DummyObject();
-        $method = new \ReflectionMethod(get_class($target), '__clone');
-        $method->setAccessible(true);
-        $method->invoke($target);
+        $this->expectException(CloneNotSupportedException::class);
+
+        clone new class
+        {
+            use \PHPJava\Packages\PHPJava\Extended\_Object;
+        };
     }
 }
