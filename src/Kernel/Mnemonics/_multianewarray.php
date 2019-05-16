14710141658419a2bcb289ee306c5a74b82dd202
memory-agape <memory.style.serv@gmail.com>
2019-05-16 15:00:44 +0900

    WIP: Add a multi dimension array generator

--- src/Kernel/Mnemonics/_multianewarray.php
+++ src/Kernel/Mnemonics/_multianewarray.php
@@ -1,6 +1,10 @@
 <?php
 namespace PHPJava\Kernel\Mnemonics;
 
+use PHPJava\Kernel\Types\_Array\Collection;
+use PHPJava\Utilities\Extractor;
+use PHPJava\Utilities\Formatter;
+
 final class _multianewarray implements OperationInterface
 {
     use \PHPJava\Kernel\Core\Accumulator;
@@ -12,9 +16,50 @@ final class _multianewarray implements OperationInterface
         $index = $this->readUnsignedShort();
         $dimensions = $this->readByte();
 
-        $descriptor = $cp[$cp[$index]->getClassIndex()]->getString();
+        $descriptor = Formatter::parseSignature(
+            $cp[$cp[$index]->getClassIndex()]->getString()
+        );
 
-        var_dump($descriptor, $dimensions);
-        exit();
+        $counts = array_fill(0, $dimensions, 0);
+
+        for ($i = $dimensions - 1; $i >= 0; $i--) {
+            $counts[$i] = Extractor::getRealValue(
+                $this->popFromOperandStack()
+            );
+        }
+
+        $data = null;
+        $multiDimensionArray = $this->makeMultiDimensionArray(
+            $data,
+            $counts,
+            $descriptor[0]['class_name'] ?? $descriptor[0]['type'],
+            0,
+            $dimensions
+        );
+
+        $this->pushToOperandStackByReference($multiDimensionArray);
+    }
+
+    /**
+     * @param $array
+     * @return $this
+     */
+    private function makeMultiDimensionArray($array, array $counts, string $type, int $currentDimension, int $maxDimension)
+    {
+        if ($currentDimension >= $maxDimension) {
+            return $array;
+        }
+        $newArray = (new Collection())->setType($type);
+        for ($i = 0; $i < $counts[$currentDimension]; $i++) {
+            $collection = (new Collection())->setType($type);
+            $newArray[$i] = $this->makeMultiDimensionArray(
+                $collection,
+                $counts,
+                $type,
+                $currentDimension + 1,
+                $maxDimension
+            );
+        }
+        return $newArray;
     }
 }
