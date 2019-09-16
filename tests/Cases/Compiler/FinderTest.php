93cd6291973ebb3a22e9fb4b17babb21a38c0257
memory-agape <memory.style.serv@gmail.com>
2019-09-16 20:31:43 +0900

    Add test

--- tests/Cases/Compiler/FinderTest.php
+++ tests/Cases/Compiler/FinderTest.php
@@ -4,6 +4,9 @@ namespace PHPJava\Tests\Cases\Compiler;
 use PHPJava\Compiler\Builder\Collection\ConstantPool;
 use PHPJava\Compiler\Builder\Finder\ConstantPoolFinder;
 use PHPJava\Compiler\Builder\Maps\EntryMap;
+use PHPJava\Compiler\Builder\Structures\Info\ClassInfo;
+use PHPJava\Compiler\Builder\Structures\Info\MethodrefInfo;
+use PHPJava\Compiler\Builder\Structures\Info\NameAndTypeInfo;
 use PHPJava\Compiler\Builder\Structures\Info\StringInfo;
 use PHPJava\Compiler\Builder\Structures\Info\Utf8Info;
 use PHPJava\Exceptions\FinderException;
@@ -44,7 +47,105 @@ class FinderTest extends Base
         $this->assertInstanceOf(StringInfo::class, $entry->getEntry());
     }
 
-    public function testFailedFind()
+    public function testSuccessfullyClassInfoInfoFind()
+    {
+        $constantPool = new ConstantPool();
+        $finder = new ConstantPoolFinder($constantPool);
+        $constantPool
+            ->add(new Utf8Info('HelloWorld'))
+            ->add(new ClassInfo(
+                $finder->find(Utf8Info::class, 'HelloWorld')
+            ));
+
+        /**
+         * @var EntryMap $entry
+         */
+        $entry = $finder->find(ClassInfo::class, 'HelloWorld')->getResult();
+        $this->assertSame(2, $entry->getEntryIndex());
+        $this->assertInstanceOf(ClassInfo::class, $entry->getEntry());
+    }
+
+    public function testSuccessfullyNameAndTypeInfoFind()
+    {
+        $constantPool = new ConstantPool();
+        $finder = new ConstantPoolFinder($constantPool);
+        $constantPool
+            ->add(new Utf8Info('main'))
+            ->add(new Utf8Info('()V'))
+            ->add(new NameAndTypeInfo(
+                $finder->find(Utf8Info::class, 'main'),
+                $finder->find(Utf8Info::class, '()V')
+            ));
+
+        /**
+         * @var EntryMap $entry
+         */
+        $entry = $finder->find(NameAndTypeInfo::class, 'main', '()V')->getResult();
+        $this->assertSame(3, $entry->getEntryIndex());
+        $this->assertInstanceOf(NameAndTypeInfo::class, $entry->getEntry());
+    }
+
+    public function testSuccessfullyMethodrefInfoFind()
+    {
+        $constantPool = new ConstantPool();
+        $finder = new ConstantPoolFinder($constantPool);
+        $constantPool
+            ->add(new Utf8Info('HelloWorld'))
+            ->add(new Utf8Info('main'))
+            ->add(new Utf8Info('()V'))
+            ->add(new ClassInfo(
+                $finder->find(Utf8Info::class, 'HelloWorld')
+            ))
+            ->add(new NameAndTypeInfo(
+                $finder->find(Utf8Info::class, 'main'),
+                $finder->find(Utf8Info::class, '()V')
+            ))
+            ->add(
+                new MethodrefInfo(
+                    $finder->find(ClassInfo::class, 'HelloWorld'),
+                    $finder->find(NameAndTypeInfo::class, 'main', '()V')
+                )
+            );
+
+        /**
+         * @var EntryMap $entry
+         */
+        $entry = $finder->find(MethodrefInfo::class, 'HelloWorld', 'main', '()V')->getResult();
+        $this->assertSame(6, $entry->getEntryIndex());
+        $this->assertInstanceOf(MethodrefInfo::class, $entry->getEntry());
+    }
+
+    public function testSuccessfullyFieldrefInfoFind()
+    {
+        $constantPool = new ConstantPool();
+        $finder = new ConstantPoolFinder($constantPool);
+        $constantPool
+            ->add(new Utf8Info('java/lang/System'))
+            ->add(new Utf8Info('out'))
+            ->add(new Utf8Info('Ljava/io/PrintStream;'))
+            ->add(new ClassInfo(
+                $finder->find(Utf8Info::class, 'java/lang/System')
+            ))
+            ->add(new NameAndTypeInfo(
+                $finder->find(Utf8Info::class, 'out'),
+                $finder->find(Utf8Info::class, 'Ljava/io/PrintStream;')
+            ))
+            ->add(
+                new MethodrefInfo(
+                    $finder->find(ClassInfo::class, 'java/lang/System'),
+                    $finder->find(NameAndTypeInfo::class, 'out', 'Ljava/io/PrintStream;')
+                )
+            );
+
+        /**
+         * @var EntryMap $entry
+         */
+        $entry = $finder->find(MethodrefInfo::class, 'java/lang/System', 'out', 'Ljava/io/PrintStream;')->getResult();
+        $this->assertSame(6, $entry->getEntryIndex());
+        $this->assertInstanceOf(MethodrefInfo::class, $entry->getEntry());
+    }
+
+    public function testFailedFindPattern1()
     {
         $constantPool = new ConstantPool();
         $finder = new ConstantPoolFinder($constantPool);
@@ -56,5 +157,19 @@ class FinderTest extends Base
          */
         $this->expectException(FinderException::class);
         $finder->find(Utf8Info::class, ':thinking:')->getResult();
+    }
+
+    public function testFailedFindPattern2()
+    {
+        $constantPool = new ConstantPool();
+        $finder = new ConstantPoolFinder($constantPool);
+        $constantPool
+            ->add(new Utf8Info('Hello World'));
+
+        /**
+         * @var EntryMap $entry
+         */
+        $this->expectException(FinderException::class);
+        $finder->find(StringInfo::class, 'Hello World')->getResult();
     }
 }
